8a776417c77944bb9144a5ae1310ba21f7b8001a
jamescarbone24 <jamescarbone24@gwu.edu>
2017-06-09 11:14:50 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('#lastPage').click(reload);
+
+	$('#home').click(reload);
+
+	$('#profile').click(reload);
+
+	$('#about').click(reload);
+
+	$('#faqs').click(reload);
+
+
+	function reload(event) {
+		event.preventDefault();
+
+		$('.container').slideUp();
+
+		$('.hide').show();    
+	}
+
+
+});
